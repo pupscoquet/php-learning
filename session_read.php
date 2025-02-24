1bf4b6311dd2fbc683213e3314e76be3e35f08d6
Victoria Coquet <pupicoquet@hotmail.com>
2025-02-24 16:43:09 +0000

    redirects, login, logout

--- session_read.php
+++ session_read.php
@@ -1,11 +1,4 @@
 <?php
 session_start();
-$users = ["david" => "password80", "clau" => "secreto"];
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST["username"];
-  $password = $_POST["password"];
-
-  // continue here > day 3 - hour 2
-}
+echo "Hello, " . $_SESSION["username"] . "!";
 ?>
